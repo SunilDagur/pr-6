13b70b5358560a57a95326de1f9f24a9628274a7
HP <HP@DESKTOP-N6A1VKE>
2023-08-21 13:56:00 +0530

    Add project files.

---
+++ pr 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define an integer array
+        int[] numbers = { 2, 4, 6, 8, 10 };
+
+        Console.WriteLine("Original Array:");
+        PrintArray(numbers);
+
+        // Specify the multiplication factor
+        int factor = 3;
+
+        // Multiply each element by the specified factor using a loop
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = numbers[i] * factor;
+        }
+
+        Console.WriteLine("Array after Multiplication:");
+        PrintArray(numbers);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
